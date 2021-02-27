7d28fcd22daf6efa254e7b9e60c45a3619feb5cd
llsspratt <72828397+llsspratt@users.noreply.github.com>
2021-02-27 13:43:02 -0500

    first commit

---
+++ Finances/Finances/ViewModels/MainVM.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Xml.Serialization;
+using Finances.Models;
+
+namespace Finances.ViewModels
+{
+    public class MainVM : INotifyPropertyChanged
+    {
+        public Posts Blog { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+    
+        public MainVM()
+        {
+            ReadRss();
+        }
+
+        public void ReadRss()
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Posts));
+            using (WebClient client = new WebClient())
+            {
+                string xml = Encoding.Default.GetString(client.DownloadData("https://www.w3schools.com/xml/simple.xml"));
+                using (Stream reader = new MemoryStream(Encoding.UTF8.GetBytes(xml)))
+                {
+                    Blog = (Posts)serializer.Deserialize(reader);
+                }
+            }
+        }
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Finances/Finances/Views/Behaviors/ListViewBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using Finances.Models;
+using Xamarin.Forms;
+
+namespace Finances.Views.Behaviors
+{
+    public class ListViewBehavior : Behavior<ListView>
+    {
+        ListView listView;
+
+        protected override void OnAttachedTo(ListView bindable)
+        {
+            base.OnAttachedTo(bindable);
+            listView = bindable;
+            listView.ItemSelected += ListView_ItemSelected;
+        }
+
+        private void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            Item selectedItem = (listView.SelectedItem) as Item;
+            Application.Current.MainPage.Navigation.PushAsync(new PostPage(selectedItem));
+        }
+
+        protected override void OnDetachingFrom(ListView bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            listView.ItemSelected -= ListView_ItemSelected;
+        }
+    }
+}
